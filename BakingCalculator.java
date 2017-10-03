3e05032c113c7f6c1388582ab59699f23f3152ad
Terry Urich <urichterry@gmail.com>
2017-10-03 11:18:08 -0700

    Changed Package variables to Integers

--- BakingCalculator.java
+++ BakingCalculator.java
@@ -19,24 +19,25 @@ public class BakingCalculator {
         double banana= loafCount*2;
         double chips = cookieCount*2.5/48;  
 
-        double flourP = (Math.ceil(flour/3.33*5));
-        double saltP = (Math.ceil(salt/6*26));
-        double baking_sodaP = (Math.ceil(baking_soda/6*16)); 
-        double vanillaP = (Math.ceil(vanilla/6)); 
-        double eggsP = (Math.ceil(eggs/12)); 
-        double sugarP = (Math.ceil(sugar/2));   
-        double chipsP = (Math.ceil(chips/2)); 
-        double butterP = (Math.ceil(butter/4));
+        int flourP = (int) (Math.ceil(flour/3.33*5));
+        int saltP = (int) (Math.ceil(salt/6*26));
+        int baking_sodaP = (int) (Math.ceil(baking_soda/6*16)); 
+        int vanillaP = (int) (Math.ceil(vanilla/6)); 
+        int eggsP = (int) (Math.ceil(eggs/12)); 
+        int sugarP = (int) (Math.ceil(sugar/2));   
+        int chipsP = (int) (Math.ceil(chips/2)); 
+        int butterP = (int) (Math.ceil(butter/4));
+        
 
         double cost = flourP*2.50 + saltP*1.09 + baking_sodaP*1.09 + vanillaP*3.99 + eggsP*1.59 + sugarP*1.99 + chipsP*3.99 + banana*0.16 + butterP*4.19;
         System.out.println(flourP + " Bag(s) of flour");
-        System.out.println(saltP + " ounces of salt");
+        System.out.println(saltP + " package(s) of salt");
         System.out.println(baking_sodaP + " box(es) of baking soda");
         System.out.println(vanillaP + " bottles of vanilla");
         System.out.println(eggsP +" Carton(s) of eggs");
         System.out.println(sugarP  +" bag(s) of sugar");
-        System.out.println(butter +" package(s) of butter");
-        System.out.println(banana +" banana(s)");
+        System.out.println(butterP +" package(s) of butter");
+        System.out.println((int) banana +" banana(s)");
         System.out.println(chipsP +" bag(s) of chocolate chips");
         System.out.println("Total cost: "+"$"+cost);   
     }
