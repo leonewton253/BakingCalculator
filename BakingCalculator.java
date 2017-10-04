19d855daa485a7969151d1b9bfcf69b4831fead7
Terry Urich <urichterry@gmail.com>
2017-10-04 11:18:02 -0700

    added more decimals

--- BakingCalculator.java
+++ BakingCalculator.java
@@ -22,19 +22,19 @@ public class BakingCalculator {
         double baking_soda = cookieCount*(1/48) + loafCount;
         double vanilla = cookieCount*(1/48) + loafCount;
         double eggs = cookieCount*(2/48) + loafCount;
-        double sugar = cookieCount*(1.5/48) + loafCount*(3/4);
+        double sugar = cookieCount*(1.5/48) + loafCount*(3.0/4);
         double butter  = cookieCount/48.0 + loafCount/3.0;
         double banana= loafCount*2;
         double chips = cookieCount*(2.5/48);  
         //Converting to packaged quantities//
         double flourP = (flour/3.33)/5;
-        double saltP = (salt/6)/26;
-        double baking_sodaP = (((baking_soda/6)/16)); 
-        double vanillaP = vanilla/6; 
-        double eggsP = eggs/12; 
-        double sugarP = sugar/2;   
-        double chipsP = chips/2; 
-        double butterP = butter/2;
+        double saltP = (salt/6.0)/26.0;
+        double baking_sodaP = (((baking_soda/6.0)/16.0)); 
+        double vanillaP = vanilla/6.0; 
+        double eggsP = eggs/12.0; 
+        double sugarP = sugar/2.0;   
+        double chipsP = chips/2.0; 
+        double butterP = butter/2.0;
          //Calculate cost of each package//
         double cost = flourP*2.50 + saltP*1.09 + baking_sodaP*1.09 + vanillaP*3.99 + eggsP*1.59 + sugarP*1.99 + chipsP*3.99 + banana*0.16 + butterP*4.19;
         //Output Shopping list with cost//
