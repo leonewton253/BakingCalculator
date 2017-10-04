ed856a7105c110bdb0cf4858731a6c5c33d6d103
Terry Urich <urichterry@gmail.com>
2017-10-03 18:40:30 -0700

    Format Cost value and fixed calculations

--- BakingCalculator.java
+++ BakingCalculator.java
@@ -1,5 +1,8 @@
 //Terrence Urich//
+import java.text.NumberFormat;
+import java.util.Locale;
 import java.util.Scanner;
+
 public class BakingCalculator {
 
     public static void main(String[] args) {
@@ -8,29 +11,31 @@ public class BakingCalculator {
         int cookieCount = s.nextInt();
         System.out.print("Enter number of Banana loafs: ");
         int loafCount = s.nextInt();
+    
         
-        double flour = cookieCount*2.25/48 +loafCount*1.5;
-        double salt = cookieCount/48 + loafCount*(1/8);
-        double baking_soda = cookieCount/48 + loafCount;
-        double vanilla = cookieCount/48 + loafCount;
-        double eggs = cookieCount*2/48 + loafCount;
-        double sugar = cookieCount*1.5/48 + loafCount*(3/4);
-        double butter  = cookieCount*2/48 + loafCount*(1/3);
+        double flour = cookieCount*(2.25/48) +loafCount*1.5;
+        double salt = cookieCount/48 + loafCount/8;
+        double baking_soda = cookieCount*(1/48) + loafCount;
+        double vanilla = cookieCount*(1/48) + loafCount;
+        double eggs = cookieCount*(2/48) + loafCount;
+        double sugar = cookieCount*(1.5/48) + loafCount*(3/4);
+        double butter  = cookieCount/48 + loafCount/3;
         double banana= loafCount*2;
-        double chips = cookieCount*2.5/48;  
+        double chips = cookieCount*(2.5/48);  
 
-        int flourP = (int) (Math.ceil(flour/3.33*5));
-        int saltP = (int) (Math.ceil(salt/6*26));
-        int baking_sodaP = (int) (Math.ceil(baking_soda/6*16)); 
-        int vanillaP = (int) (Math.ceil(vanilla/6)); 
-        int eggsP = (int) (Math.ceil(eggs/12)); 
-        int sugarP = (int) (Math.ceil(sugar/2));   
-        int chipsP = (int) (Math.ceil(chips/2)); 
-        int butterP = (int) (Math.ceil(butter/4));
+        double flourP = (flour/3.33)/5;
+        double saltP = (salt/6)/26;
+        double baking_sodaP = (((baking_soda/6)/16)); 
+        double vanillaP = vanilla/6; 
+        double eggsP = eggs/12; 
+        double sugarP = sugar/2;   
+        double chipsP = chips/2; 
+        double butterP = butter/2;
         
 
         double cost = flourP*2.50 + saltP*1.09 + baking_sodaP*1.09 + vanillaP*3.99 + eggsP*1.59 + sugarP*1.99 + chipsP*3.99 + banana*0.16 + butterP*4.19;
         System.out.println("Shopping list for "+cookieCount+" cookie(s)"+" and "+loafCount+" loaf(s) of bread:");
+        System.out.println();
         System.out.println(flourP + " Bag(s) of flour");
         System.out.println(saltP + " package(s) of salt");
         System.out.println(baking_sodaP + " box(es) of baking soda");
@@ -40,7 +45,12 @@ public class BakingCalculator {
         System.out.println(butterP +" package(s) of butter");
         System.out.println((int) banana +" banana(s)");
         System.out.println(chipsP +" bag(s) of chocolate chips");
-        System.out.println("Total cost: "+"$"+cost);   
+        System.out.println();  
+        NumberFormat formatter = NumberFormat.getCurrencyInstance();
+        System.out.println("Total cost: " + formatter.format(cost));
+
+        
+
     }
 
 }
