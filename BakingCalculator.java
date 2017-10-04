3fb43edd4333ccbab4dc1a46f64662b83c181e13
Terry Urich <urichterry@gmail.com>
2017-10-04 10:49:39 -0700

    Cleaned up code for assignment and added comments

--- BakingCalculator.java
+++ BakingCalculator.java
@@ -6,13 +6,17 @@ import java.util.Scanner;
 public class BakingCalculator {
 
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.print("Enter number of cookies: ");
-        int cookieCount = s.nextInt();
-        System.out.print("Enter number of Banana loafs: ");
-        int loafCount = s.nextInt();
-    
+        int cookieCount= 24;
+        int loafCount = 1;
+        //Scanner functionality//
         
+        //Scanner s = new Scanner(System.in);
+        //System.out.print("Enter number of cookies: ");
+        //int cookieCount = s.nextInt();
+        //System.out.print("Enter number of Banana loafs: ");
+        //int loafCount = s.nextInt();
+    
+        //Recipes//
         double flour = cookieCount*(2.25/48) +loafCount*1.5;
         double salt = cookieCount/48 + loafCount/8;
         double baking_soda = cookieCount*(1/48) + loafCount;
@@ -22,7 +26,7 @@ public class BakingCalculator {
         double butter  = cookieCount/48 + loafCount/3;
         double banana= loafCount*2;
         double chips = cookieCount*(2.5/48);  
-
+        //Converting to packaged quantities//
         double flourP = (flour/3.33)/5;
         double saltP = (salt/6)/26;
         double baking_sodaP = (((baking_soda/6)/16)); 
@@ -32,8 +36,9 @@ public class BakingCalculator {
         double chipsP = chips/2; 
         double butterP = butter/2;
         
-
+        //Calculate cost of each package//
         double cost = flourP*2.50 + saltP*1.09 + baking_sodaP*1.09 + vanillaP*3.99 + eggsP*1.59 + sugarP*1.99 + chipsP*3.99 + banana*0.16 + butterP*4.19;
+        //Output Shopping list with cost//
         System.out.println("Shopping list for "+cookieCount+" cookie(s)"+" and "+loafCount+" loaf(s) of bread:");
         System.out.println();
         System.out.println((int) Math.ceil(flourP) + " Bag(s) of flour");
